0f9358b0cd0fb1c89ee6791a8643d672821b8def
jean1garcia <jeangarciaglez@gmail.com>
2020-09-17 13:43:25 +0000

    Added cool verbs and nouns

---
+++ js/nouns/JAJ.js
@@ -0,0 +1,7 @@
+nouns = nouns.concat([
+    'fireperson',
+    'truck',
+    'Trump',
+    'Dwight Schrute',
+    'Batman and Robin',
+    ]);
---
+++ js/verbs/JAJ.js
@@ -0,0 +1,14 @@
+verbs = verbs.concat([
+    'knitt',
+    'brunch',
+    'travel',
+    'study',
+    'vomit',
+    'laugh',
+    'cry',
+    'party',
+    'clean',
+    'cook',
+    'yell',
+    'bett',
+]);
